71651c9de2ac5c35b16ba55236a30b7ea9d92906
Vedika Maheshwari <vedika@my.yorku.ca>
2024-03-25 00:09:08 -0400

    Updated the EmailObj struct

--- src/emails/structs.go
+++ src/emails/structs.go
@@ -7,9 +7,9 @@ import (
 // Represents documents in the Emails collection
 type EmailObj struct {
 	Id      primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-	Name string `json:"name" bson:"Name"`
-	Type string `json:"type" bson:"Type"`
-	Company string `json:"company" bson:"Company"`
+	Name    string             `json:"name" bson:"Name"`
+	Type    string             `json:"type" bson:"Type"`
+	Company string             `json:"company" bson:"Company"`
 	Subject string             `json:"subject" bson:"Subject"`
 	Body    string             `json:"body" bson:"Body"`
 }
